be33c83cd404bee240261571d84b1e41021d467e
Slawomir Grabowski <grabowski@quickturnstudio.com>
2020-02-20 10:15:06 +0100

    Adding Google Test and tests module

---
+++ src/test/ExampleFileTest.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+
+#include <Card.h>
+
+namespace texasHolderPoker::test
+{
+
+TEST(ExampleTestSuite, ExampleTest)
+{
+    // test if we link properly to testing lib
+    Card card;
+}
+
+} // namespace texasHolderPoker::test
